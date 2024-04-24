6df452149c63333edc743e7ced3c872d3d80a22b
jo_sung <zounsam@naver.com>
2024-04-24 19:25:34 +0900

    test

---
+++ src/main/java/com/sungyeon/SungyeonApplication.java
@@ -0,0 +1,13 @@
+package com.sungyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SungyeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SungyeonApplication.class, args);
+    }
+
+}
